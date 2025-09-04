5cf3081bf35b5a7ea5b8748fa861d7bb1d19f358
fernandoggaitan <fernandoggaitan@gmail.com>
2025-09-04 13:47:12 -0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+use App\Models\User;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('test', function () {
+    $usuario = User::create([
+        'name' => 'Juanito',
+        'email' => 'juanito@davinci.edu.ar',
+        'password' => '1234'
+    ]);
+    return "Se creó un usuario con el ID: {$usuario->id}";
+});
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
